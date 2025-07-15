21e65abeb7d56531b9479f3781c3696211b16b4f
shyang <shyang@inzisoft.com>
2025-07-15 10:56:08 +0900

    프로젝트 최초 커밋

---
+++ src/main/java/com/sprinboottemplate/springboottempate/SpringbootTempateApplication.java
@@ -0,0 +1,13 @@
+package com.sprinboottemplate.springboottempate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTempateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTempateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprinboottemplate/springboottempate/SpringbootTempateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprinboottemplate.springboottempate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTempateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
